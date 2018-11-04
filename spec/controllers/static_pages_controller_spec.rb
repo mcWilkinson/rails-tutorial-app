0b4d4b99b038bdf846d96cb5963ae6540526177c
Connor W <connor42.w@gmail.com>
2018-11-04 00:22:04 +0000

    Changed 'help' to 'contact'

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,19 +9,17 @@ RSpec.describe StaticPagesController, type: :controller do
     end
   end
 
-  describe "GET #help" do
-    it "returns http success" do
-      get :help
-      expect(response).to have_http_status(:success)
-    end
-  end
-  
   describe "GET #about" do
     it "returns http success" do
       get :about
       expect(response).to have_http_status(:success)
     end
   end
-
-
+  
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
